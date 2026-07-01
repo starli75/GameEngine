8f3f9fbc09147b3a4ca9d2ed49c56980eb708b6f
starli75 <vvega@student.neumont.edu>
2026-07-01 09:51:03 -0600

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,12 @@
+// Engine.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+#include <iostream>
+
+void fnEngine()
+{
+	std::cout << "welcome to the library. \n";
+}
---
+++ Game/Game.cpp
@@ -0,0 +1,62 @@
+#include "../Engine/Engine.cpp"
+#include "SDL3/SDL.h"
+
+#include <iostream>
+
+
+int main()
+{   
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("SDL3 Project", 1280, 1024, 0);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == nullptr) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event e;
+    bool quit = false;
+
+    // Define a rectangle
+    SDL_FRect greenSquare{ 270, 190, 200, 200 };
+
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set render draw color to black
+        SDL_RenderClear(renderer); // Clear the renderer
+
+        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
+        SDL_RenderPoint(renderer, rand() % 1280, rand() % 1024);
+
+        SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 255);
+        SDL_RenderFillRect(renderer, &greenSquare); // Render the rectangle
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDebugText(renderer, 10, 10, "Hello World!");
+
+        SDL_RenderPresent(renderer); // Render the screen
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
+
